a4adaf187be61fd0bc04253bf919cf74c19428e0
964rakeshkumar <31364789+964rakeshkumar@users.noreply.github.com>
2022-03-15 18:49:54 +0530

    Create migrate_tasks.groovy

--- files/groovy/migrate_tasks.groovy
+++ files/groovy/migrate_tasks.groovy
@@ -6,6 +6,7 @@ import org.sonatype.nexus.scheduling.schedule.Monthly
 import org.sonatype.nexus.scheduling.schedule.Schedule
 import org.sonatype.nexus.scheduling.schedule.Weekly
 import java.text.SimpleDateFormat
+import java.util.*;
 
 def fileName = 'tasks.yaml'
 def migrationTasks = ['nexus_scheduled_tasks':[]]
@@ -18,59 +19,83 @@ Map<String,String> currentTask =[:]
 Schedule currentTaskScheduleType = rtTaks.getSchedule()
 
 scheduleType = currentTaskScheduleType.getType()
-currentTask.put('shedule_type',scheduleType)
 if(scheduleType == 'daily' || scheduleType== 'hourly' || scheduleType == 'once'){
+    currentTask.put('schedule_type',scheduleType)
     schedule_startAt = currentTaskScheduleType.getStartAt()
     currentTask.put('schedule_startAt',schedule_startAt)
 }
 if(scheduleType == 'weekly'){
+    currentTask.put('schedule_type',scheduleType)
     weekly_days = currentTaskScheduleType.getDaysToRun()
     currentTask.put('weekly_days',weekly_days)
 }
 if(scheduleType == 'monthly'){
+    currentTask.put('schedule_type',scheduleType)
     monthly_days = currentTaskScheduleType.getDaysToRun()
-    currentTask.put('monthly_days',monthly_days)
+    def mDay =[]
+    monthly_days.each { dayVal ->
+        mDay.add(dayVal['day'])
+    }
+    currentTask.put('monthly_days',mDay)
 }
 if(scheduleType == 'cron'){
     schedule_cronExpression = currentTaskScheduleType.getCronExpression()
-    currentTask.put('schedule_cronExpression',schedule_cronExpression)
+    currentTask.put('cron',schedule_cronExpression.toString())
+}
+if(scheduleType == 'manual'){
+    currentTask.put('schedule_type',scheduleType)
+}
+if(scheduleType =='now'){
+    currentTask.put('schedule_type',scheduleType)
 }
 TaskConfiguration currentTaskConfiguration = rtTaks.getConfiguration()
 
 tasksName = currentTaskConfiguration.getName()
 // tasksId = currentTaskConfiguration.getId() not requried 
 
-tasksTypeId = currentTaskConfiguration.getTypeId()
-tasksTypeName = currentTaskConfiguration.getTypeName()
-tasksAleartMail = currentTaskConfiguration.getAlertEmail()
-tasksNotification = currentTaskConfiguration.getNotificationCondition()
-tasksRepository = currentTaskConfiguration.getString('repositoryName')
-tasksblobstoreName = currentTaskConfiguration.getString('blobstoreName')
-tasklastUsed = currentTaskConfiguration.getString('lastUsed')
-tasksgroupId = currentTaskConfiguration.getString('groupId')
-tasksartifactId = currentTaskConfiguration.getString('artifactId')
-taskbaseVersion = currentTaskConfiguration.getString('baseVersion')
-tasksrebuildChecksums = currentTaskConfiguration.getString('rebuildChecksums')
-taskyumMetadataCaching = currentTaskConfiguration.getString('yumMetadataCaching')
-tasklocation = currentTaskConfiguration.getString('location')
-tasksAge = currentTaskConfiguration.getString('age')
-taskSource = currentTaskConfiguration.getString('source')
-taskDryrun = currentTaskConfiguration.getString('dryRun')
-taskrestoreBlobMetadata = currentTaskConfiguration.getString('restoreBlobMetadata')
-taskunDeleteReferencedBlobs=currentTaskConfiguration.getString('unDeleteReferencedBlobs')
-taskintegrityCheck=currentTaskConfiguration.getString('integrityCheck')
+String tasksTypeId = currentTaskConfiguration.getTypeId()
+String tasksTypeName = currentTaskConfiguration.getTypeName()
+String tasksAleartMail = currentTaskConfiguration.getAlertEmail()
+String tasksNotification = currentTaskConfiguration.getNotificationCondition()
+String tasksRepository = currentTaskConfiguration.getString('repositoryName')
+String tasksblobstoreName = currentTaskConfiguration.getString('blobstoreName')
+String tasklastUsed = currentTaskConfiguration.getString('lastUsed')
+String tasksgroupId = currentTaskConfiguration.getString('groupId')
+String tasksartifactId = currentTaskConfiguration.getString('artifactId')
+String taskbaseVersion = currentTaskConfiguration.getString('baseVersion')
+String tasksrebuildChecksums = currentTaskConfiguration.getString('rebuildChecksums')
+String taskyumMetadataCaching = currentTaskConfiguration.getString('yumMetadataCaching')
+String tasklocation = currentTaskConfiguration.getString('location')
+String tasksAge = currentTaskConfiguration.getString('age')
+String taskSource = currentTaskConfiguration.getString('source')
+String taskDryrun = currentTaskConfiguration.getString('dryRun')
+String taskrestoreBlobMetadata = currentTaskConfiguration.getString('restoreBlobMetadata')
+String taskunDeleteReferencedBlobs=currentTaskConfiguration.getString('unDeleteReferencedBlobs')
+String taskintegrityCheck=currentTaskConfiguration.getString('integrityCheck')
+String taskLanguage = currentTaskConfiguration.getString('language')
+
 
 def taskproperty = [:]
 def boolproperty = [:]
-
+if(tasksTypeId =='epository.docker.upload-purge'){
+    taskproperty.put('age',tasksAge)
+}
+if(tasksTypeId == 'repository.maven.remove-snapshots'){
+    taskproperty.put('minimumRetained',currentTaskConfiguration.getString('minimumRetained'))
+    //taskproperty.put('minimumRetained',currentTaskConfiguration.getString('integrityCheck'))
+    taskproperty.put('snapshotRetentionDays',currentTaskConfiguration.getString('snapshotRetentionDays'))
+    taskproperty.put('gracePeriodInDays',currentTaskConfiguration.getString('gracePeriodInDays'))
+    boolproperty.put('removeIfReleased',currentTaskConfiguration.getString('removeIfReleased'))
+}
 if(tasksTypeId == 'blobstore.compact' || tasksTypeId =='security.purge-api-keys' || tasksTypeId=='blobstore.rebuildComponentDB'){
     taskproperty.put('blobstoreName',tasksblobstoreName)
 }
-if(tasksTypeId == 'repository.maven.purge-unused-snapshots') {
+if(tasksTypeId == 'repository.maven.purge-unused-snapshots' || tasksTypeId =='repository.purge-unused') {
     taskproperty.put('lastUsed',tasklastUsed)
 }
 if(tasksTypeId =='script'){
     taskproperty.put('source',taskSource)
+    taskproperty.put('language',taskLanguage)
 }
 if(tasksTypeId == 'repository.maven.rebuild-metadata'){
     taskproperty.put('groupId',tasksgroupId)
